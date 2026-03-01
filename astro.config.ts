4e0c349769ef9216deba193b3ba1688fc184698d
thienguen <nvktvan@gmail.com>
2026-02-28 22:31:19 -0800

    💮 ui: navigation and about page

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,16 @@
+import mdx from '@astrojs/mdx'
+import vercel from '@astrojs/vercel'
 import { defineConfig } from 'astro/config'
 
-import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+import rehypeShiki from '@shikijs/rehype'
 import rehypeExpressiveCode from 'rehype-expressive-code'
 import rehypeExternalLinks from 'rehype-external-links'
 import rehypeKatex from 'rehype-katex'
-import rehypeShiki from '@shikijs/rehype'
 import remarkEmoji from 'remark-emoji'
 import remarkMath from 'remark-math'
 
@@ -21,9 +22,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://vankythien.dev',
+  output: 'server',
+  adapter: vercel(),
   integrations: [mdx(), react(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ['rough-notation'],
+    },
   },
   server: {
     port: 1234,
